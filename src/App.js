1b0fe6950c6eecac363f72139cdbf610741a871e
hasnatsajid <hasnatsajid965@gmail.com>
2022-08-20 15:09:05 +0500

    added calculator react boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="main">
+        <section class="display"></section>
+
+        <div class="container">
+          <button class="key trash keymod">
+            <i class="fas fa-trash"></i>
+          </button>
+          <button class="key percent keymod">
+            <i class="fas fa-percent"></i>
+          </button>
+          <button class="key backspace keymod">
+            <i class="fas fa-backspace"></i>
+          </button>
+          <button class="key divide keymod">
+            <i class="fas fa-divide"></i>
+          </button>
+        </div>
+
+        <div class="container">
+          <button class="key seven numkey">7</button>
+          <button class="key eight numkey">8</button>
+          <button class="key nine numkey">9</button>
+          <button class="key keymod multiply">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+
+        <div class="container">
+          <button class="key four numkey">4</button>
+          <button class="key five numkey">5</button>
+          <button class="key six numkey">6</button>
+          <button class="key keymod minus">
+            <i class="fas fa-minus"></i>
+          </button>
+        </div>
+
+        <div class="container">
+          <button class="key one numkey">1</button>
+          <button class="key two numkey">2</button>
+          <button class="key three numkey">3</button>
+          <button class="key keymod add">
+            <i class="fas fa-plus"></i>
+          </button>
+        </div>
+
+        <div class="container">
+          <button class="key null numkey">0</button>
+          <button class="key zero numkey">0</button>
+          <button class="key dot numkey">
+            <i class="fas fa-circle"></i>
+          </button>
+          <button class="key keymod equals">
+            <i class="fas fa-equals"></i>
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
